a80d422cb0c718dd2b63b20767af5d5f8887d57c
darren277 <darren277@yahoo.com>
2025-01-22 03:31:21 -0800

    Lights endpoints.

--- app.py
+++ app.py
@@ -13,5 +13,21 @@ def index():
         """
     )
 
+@app.route('/lights')
+def lights():
+    return open('lights/index.html').read()
+
+@app.route('/lights/index.js')
+def lights_js():
+    return open('lights/index.js', encoding='utf-8').read()
+
+@app.route('/lights/data.json')
+def lights_data():
+    #return open('lights/data.json').read()
+    from lights.convert import conv
+    #data = conv('lights/Starry_Night.jpg')
+    data = conv()
+    return jsonify(data)
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5041)
